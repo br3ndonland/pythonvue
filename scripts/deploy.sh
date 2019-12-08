78ca6e1a581177ddc7cbf3e7a8fc6fb98f9eb068
Brendon Smith <br3ndonland@protonmail.com>
2019-12-08 01:12:26 -0500

    Create Docker network before deploying swarm
    When running deploy.sh, the following error was seen:
    network "traefik-public" is declared as external, but could not be found
    . You need to create a swarm-scoped network before the stack is deployed
    
    This commit will add a docker network create command to deploy.sh:
    `docker network create --driver=overlay traefik-public`
    
    The command was based on https://dockerswarm.rocks/traefik/.
    
    See commit a0ee1a34 in br3ndonland/full-stack-fastapi-postgresql
    https://github.com/br3ndonland/full-stack-fastapi-postgresql

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -4,21 +4,21 @@
 set -e
 
 DOMAIN=${DOMAIN} \
-TRAEFIK_TAG=${TRAEFIK_TAG} \
-STACK_NAME=${STACK_NAME} \
-TAG=${TAG} \
-docker-compose \
--f docker-compose.shared.admin.yml \
--f docker-compose.shared.base-images.yml \
--f docker-compose.shared.depends.yml \
--f docker-compose.shared.env.yml \
--f docker-compose.deploy.command.yml \
--f docker-compose.deploy.images.yml \
--f docker-compose.deploy.labels.yml \
--f docker-compose.deploy.networks.yml \
--f docker-compose.deploy.volumes-placement.yml \
-config > docker-stack.yml
+  TRAEFIK_TAG=${TRAEFIK_TAG} \
+  STACK_NAME=${STACK_NAME} \
+  TAG=${TAG} \
+  docker-compose \
+  -f docker-compose.shared.admin.yml \
+  -f docker-compose.shared.base-images.yml \
+  -f docker-compose.shared.depends.yml \
+  -f docker-compose.shared.env.yml \
+  -f docker-compose.deploy.command.yml \
+  -f docker-compose.deploy.images.yml \
+  -f docker-compose.deploy.labels.yml \
+  -f docker-compose.deploy.networks.yml \
+  -f docker-compose.deploy.volumes-placement.yml \
+  config >docker-stack.yml
 
 docker-auto-labels docker-stack.yml
-
-docker stack deploy -c docker-stack.yml --with-registry-auth ${STACK_NAME}
+docker network create --driver=overlay traefik-public
+docker stack deploy -c docker-stack.yml --with-registry-auth "${STACK_NAME}"
